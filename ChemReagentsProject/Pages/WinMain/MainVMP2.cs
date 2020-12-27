f06e30840b32e05afd4ff6b22f3e955abcc8219c
Инар Инарыч <Inar2000s@mail.ru>
2020-12-27 19:11:01 +0300

    Пытка2

--- ChemReagentsProject/Pages/WinMain/MainVMP2.cs
+++ ChemReagentsProject/Pages/WinMain/MainVMP2.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace ChemReagentsProject.Pages.WinMain
+namespace ChemReagentsProject.ViewModel
 {
     partial class MainVM
     {
