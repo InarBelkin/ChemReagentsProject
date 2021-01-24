ec84a30dc510f4f77f968ba9070d03421c647f21
Инар Инарыч <Inar2000s@mail.ru>
2021-01-24 14:04:09 +0300

    не знаю что

---
+++ BLL/Interfaces/ICRUDRepos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface ICrudRepos<T>
+    {
+        ObservableCollection<T> GetList();
+        T GetItem(int id);
+        Exception Create(T item);
+        Exception Update(T item);
+        void Delete(int id);
+    }
+}
